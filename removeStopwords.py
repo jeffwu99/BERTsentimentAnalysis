0aae241831cf5fd983562241dc6af01f9908c63a
Jeff Wu <jeffreywu1999@gmail.com>
2022-02-18 13:10:45 -0800

    added exception handling

--- removeStopwords.py
+++ removeStopwords.py
@@ -1,28 +1,26 @@
 import nltk
 from nltk.corpus import stopwords
-#nltk.download('stopwords')
+nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
-#nltk.download('punkt')
-
-
+nltk.download('punkt')
 
 stop_words = set(stopwords.words('english'))
 
-FILENAME = "amazonReviewData/reviewTexts.txt"
-OUTPUT = "amazonReviewData/stopWordedText.txt"
-f = open(FILENAME, "r")
-w = open(OUTPUT, "w")
+FILENAME = "data/reviewTexts.txt"
+OUTPUT = "data/reviewTextsWithoutStops.txt"
 
+try:
+  f = open(FILENAME, "r")
+  w = open(OUTPUT, "w")
 
-
-count = 0
-list_of_strings = f.readlines()
-for line in list_of_strings:
-  word_tokens = word_tokenize(line)
-  without_sw = [word for word in word_tokens if not word in stop_words]
-  str = ""
-  for element in without_sw:
-    str = str + element + " "
-  w.write(str + '\n')
-  count += 1
-  print(count)
+  count = 0
+  list_of_strings = f.readlines()
+  for line in list_of_strings:
+    word_tokens = word_tokenize(line)
+    without_sw = [word for word in word_tokens if not word in stop_words]
+    str = ""
+    for element in without_sw:
+      str = str + element + " "
+    w.write(str + '\n')
+except FileNotFoundError:
+  print("reviewTexts.txt cannot be found")
