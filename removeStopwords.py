f179866b8ab65fdcc9537b5b2a3247bfddebc2a2
jeffwu99 <JeffreyWu1999@gmail.com>
2022-02-28 15:59:02 -0800

    update

--- removeStopwords.py
+++ removeStopwords.py
@@ -1,26 +1,15 @@
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-from nltk.tokenize import word_tokenize
-nltk.download('punkt')
+from gensim.parsing.preprocessing import remove_stopwords
 
-stop_words = set(stopwords.words('english'))
-
-FILENAME = "data/reviewTexts.txt"
-OUTPUT = "data/reviewTextsWithoutStops.txt"
+FILENAME = "data/dataFinal.json"
+OUTPUT = "data/reviewTextsWithoutStops.json"
 
 try:
   f = open(FILENAME, "r")
   w = open(OUTPUT, "w")
 
-  count = 0
   list_of_strings = f.readlines()
   for line in list_of_strings:
-    word_tokens = word_tokenize(line)
-    without_sw = [word for word in word_tokens if not word in stop_words]
-    str = ""
-    for element in without_sw:
-      str = str + element + " "
+    str = remove_stopwords(line)
     w.write(str + '\n')
 except FileNotFoundError:
   print("reviewTexts.txt cannot be found")
